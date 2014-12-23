02b174047a4df2bd049912ec199c5cb7bfe848d8
Steven Watkins <steven.watkins78@gmail.com>
2014-12-22 23:57:27 -0500

    Created test to verify error shows when description is less than 10 characters.

--- spec/features/books/user_posts_link_to_book_spec.rb
+++ spec/features/books/user_posts_link_to_book_spec.rb
@@ -8,14 +8,14 @@ So I can share what I'm reading with others
 
 Acceptance Criteria
 
-[ ] I must provide a title that is at least 1 character long
-[ ] I must provide a description that is at least 10 characters long
+[x] I must provide a title that is at least 1 character long
+[x] I must provide a description that is at least 10 characters long
 [ ] I must provide a valid URL to the book
 [ ] I must be presented with errors if I fill out the form incorrectly
 
 ) do
 
-  def create_user_and_sign_in
+  def create_user_and_sign_in_and_click_post_a_book
     user = FactoryGirl.create(:user)
 
     visit root_path
@@ -24,6 +24,11 @@ Acceptance Criteria
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button "Log in"
+
+    expect(page).to have_content "Signed in successfully."
+    expect(page).to have_content "Post a Book"
+
+    click_link "Post a Book"
   end
 
   context "user successfully posts link to a book" do
@@ -60,12 +65,8 @@ Acceptance Criteria
   context 'user makes mistakes when posting book' do
 
     scenario "User leaves title field blank (less than 1 character)" do
-      create_user_and_sign_in
 
-      expect(page).to have_content "Signed in successfully."
-      expect(page).to have_content "Post a Book"
-
-      click_link "Post a Book"
+      create_user_and_sign_in_and_click_post_a_book
 
       fill_in "Title", with: ""
       fill_in "Author", with: "John Steinbeck"
@@ -76,6 +77,22 @@ Acceptance Criteria
 
       expect(page).to have_content "Fill out the forms below to add book to your collection."
       expect(page).to have_content "Title can't be blank"
+
+    end
+
+    scenario "User enters description that is too short" do
+
+      create_user_and_sign_in_and_click_post_a_book
+
+      fill_in "Title", with: "Of mice and men"
+      fill_in "Author", with: "John Steinbeck"
+      fill_in "Description", with: "A book"
+      fill_in "URL", with: "http://www.sparknotes.com/lit/micemen/"
+
+      click_button "Submit"
+
+      expect(page).to have_content "Fill out the forms below to add book to your collection."
+      expect(page).to have_content "Description is too short (minimum is 10 characters)"
 
     end
 
